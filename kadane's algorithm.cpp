c3c4aec76fddac0498c7243b480fc931977da83f
P.Suraj <srjsunny123@gmail.com>
2019-03-18 00:17:39 +0530

    maximum sum subarray

---
+++ kadane's algorithm.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+//practice problem : https://leetcode.com/problems/maximum-subarray/
+
+
+
+
+int kadane(int a[],int n)
+{
+//initially it's the first element
+int previousBest = a[0];
+int ans = a[0];
+
+//start from next element
+for(int i=1;i<n;i++)
+{
+//either start a new series or continue the subarray with this element
+previousBest = max(a[i],a[i]+previousBest);
+
+if(previousBest>ans)
+    ans = previousBest;
+
+}
+return ans;
+}
+
+int main()
+{
+int arr[]= {-2, -3, 4, -1, -2, 1, 5, -3};
+
+int n = sizeof(arr)/sizeof(arr[0]);
+
+int maxSum = kadane(arr,n);
+
+cout<<maxSum;
+
+
+}
