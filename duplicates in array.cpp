6031773bc912808044bae48655a92e79cc2872d4
P.Suraj <srjsunny123@gmail.com>
2019-04-04 11:57:30 +0530

    O(n) time complexity

--- duplicates in array.cpp
+++ duplicates in array.cpp
@@ -1,3 +1,6 @@
+//not considering output array in final output
+
+
 #include<bits/stdc++.h>
 using namespace std;
 int main()
@@ -6,12 +9,18 @@ int main()
 	
 	/*
 	2 , 1 , 2 , 1
+	so first we go to 2, then make the element at that index i.e. 1 , negative ->means mark the element as seen
 	
+	2, -1, 2, 1
+	now we go to 1, then make the element at that index i.e. 2 , negative
+	
+	-2, -1, 2, 1
+	now we go to 2, but we see that the element at that index is -ve means element already seen, so this should be the repeated one
 	
 	*/
 	vector<int> output;
 	for(int i=0;i<v.size();i++)
-	{
+	{      //here subtracting one because zero based indexing
 		int value_at_index = v[abs(v[i])-1];
 		
 		//if negative means we have already seen that element, so this element should be the repeated one.
