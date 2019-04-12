8ce4f832712227ec3c70e38e09613a202134f9d5
P.Suraj <srjsunny123@gmail.com>
2019-04-13 01:39:21 +0530

    Create selling and buying stock 1.cpp

---
+++ selling and buying stock 1.cpp
@@ -0,0 +1,24 @@
+/* find max profit we can make buying and selling only one time*/
+/* The logic is we find the minimum buying prices , subtract with the current price and take the maximum from it  
+*/
+
+
+
+
+        int profit = 0;
+        int min_buy = INT_MAX;
+        
+        for(int i=0;i<prices.size();i++)
+        {    //if the prices is less than the buying one update it
+            if(prices[i]<min_buy)
+                    min_buy=prices[i];
+            
+            else
+            {   //we take the maximum of current profit , or the price[i] minus the minimum buy
+                profit= max(profit,prices[i]-min_buy);
+            }
+        }
+        
+        return profit;
+        
+        
